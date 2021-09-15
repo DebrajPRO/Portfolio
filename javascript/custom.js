2daae13fb976135e927a98766061a380ae180f2b
Debraj Sikdar <debrajsikdar.personal@gmail.com>
2021-09-15 21:23:24 +0530

    Complete

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
 
   $('header #toggle-btn').click(function(){
-    $('header nav').slideToggle(1000);
+    $('header nav').slideToggle(500);
   });
 
   $(".p-reload").click(function(){
