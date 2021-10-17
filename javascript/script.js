ec3c508822ee491c3fe169a5ac246faf790a3a70
Debraj Sikdar <debrajsikdar.personal@gmail.com>
2021-10-17 20:31:49 +0530

    update

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,19 @@
 "use strict";
 
+/////// loading animation ///////
+
+let screenloader = document.getElementById('loading');
+
+function loadscreen(){
+	screenloader.style.display = 'none';
+};
+
+
+///////////////////////////////////////
+
+
+///// Toggle Menu Bar Start //////
+
 let collapseNav = document.getElementById('toggle-btn');
 
 collapseNav.onclick = function(){
